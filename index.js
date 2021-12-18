f39ceb0969f9f4a79f559917231d891e75d27eaf
szympajka <szymon.pajka@gmail.com>
2021-12-18 16:49:56 +0000

    new components

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import Button from "./Button/Button";
-
-export default Button;
+export { default as Button } from './Button/Button';
+export { default as Button2 } from './Button2/Button'
+export { default as Button3 } from './Button3/Button';
